5cb1d73c7182718dc1f4d06433b8b00f9f1fe4c7
grrtyre <104871533+grrtyre@users.noreply.github.com>
2026-07-12 01:56:53 +0800

    sticky-notes-manager v1.1.0: add recycle bin (30-day auto cleanup), fix download links, 129 unit tests

--- sticky-notes-manager/src/preload.js
+++ sticky-notes-manager/src/preload.js
@@ -5,8 +5,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('notesAPI', {
   load: () => ipcRenderer.invoke('notes:load'),
-  save: (notes) => ipcRenderer.invoke('notes:save', notes),
+  save: (notes, trash) => ipcRenderer.invoke('notes:save', notes, trash),
   exportNotes: (notes) => ipcRenderer.invoke('notes:export', notes),
   importNotes: () => ipcRenderer.invoke('notes:import'),
-  onAction: (callback) => ipcRenderer.on('action', (event, action) => callback(action))
+  onAction: (callback) => ipcRenderer.on('action', (event, action) => callback(action)),
+  // 回收站
+  restoreFromTrash: (id) => ipcRenderer.invoke('trash:restore', id),
+  deleteFromTrash: (id) => ipcRenderer.invoke('trash:delete', id),
+  emptyTrash: () => ipcRenderer.invoke('trash:empty')
 });
