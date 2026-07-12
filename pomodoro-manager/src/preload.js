b8bf9065050999e95753645638be778419cc2f9d
grrtyre <104871533+grrtyre@users.noreply.github.com>
2026-07-12 19:10:23 +0800

    新增 番茄管家：本地优先的番茄工作法专注桌面应用

---
+++ pomodoro-manager/src/preload.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  getState: () => ipcRenderer.invoke('state:get'),
+  start: () => ipcRenderer.invoke('timer:start'),
+  pause: () => ipcRenderer.invoke('timer:pause'),
+  resume: () => ipcRenderer.invoke('timer:resume'),
+  reset: () => ipcRenderer.invoke('timer:reset'),
+  skip: () => ipcRenderer.invoke('timer:skip'),
+  addTask: (title, estimate) => ipcRenderer.invoke('task:add', title, estimate),
+  setCurrentTask: (id) => ipcRenderer.invoke('task:current', id),
+  completeTask: (id) => ipcRenderer.invoke('task:complete', id),
+  deleteTask: (id) => ipcRenderer.invoke('task:delete', id),
+  saveConfig: (cfg) => ipcRenderer.invoke('config:save', cfg),
+  exportData: () => ipcRenderer.invoke('data:export'),
+  importData: (json) => ipcRenderer.invoke('data:import', json),
+  onTick: (cb) => ipcRenderer.on('tick', (e, data) => cb(data)),
+  onPhase: (cb) => ipcRenderer.on('phase', (e, data) => cb(data)),
+  onNotify: (cb) => ipcRenderer.on('notify', (e, data) => cb(data))
+});
